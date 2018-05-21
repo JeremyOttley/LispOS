cc3b1ac8369c8a32857ba66fcc1e6f2710bb044d
Gazaunga <Gazaunga@gmail.com>
2018-05-21 08:36:22 -0700

    Create desktop-configuration.scm

---
+++ desktop-configuration.scm
@@ -0,0 +1,54 @@
+;; This is an operating system configuration template
+;; for a "desktop" setup without full-blown desktop
+;; environments.
+
+(use-modules (gnu) (gnu system nss))
+(use-service-modules desktop)
+(use-package-modules bootloaders certs ratpoison suckless wm)
+
+(operating-system
+  (host-name "LispOS")
+  (timezone "America/New York")
+  (locale "en_US.utf8")
+
+  ;; Use the UEFI variant of GRUB with the EFI System
+  ;; Partition mounted on /boot/efi.
+  (bootloader (bootloader-configuration
+                (bootloader grub-efi-bootloader)
+                (target "/boot/efi")))
+
+  ;; Assume the target root file system is labelled "my-root",
+  ;; and the EFI System Partition has UUID 1234-ABCD.
+  (file-systems (cons* (file-system
+                         (device "gazbit-root")
+                         (title 'label)
+                         (mount-point "/")
+                         (type "ext4"))
+                       (file-system
+                         (device (uuid "1234-ABCD" 'fat))
+                         (title 'uuid)
+                         (mount-point "/boot/efi")
+                         (type "vfat"))
+                       %base-file-systems))
+
+  (users (cons (user-account
+                (name "gazbit")
+                (comment "Comrade")
+                (group "users")
+                (supplementary-groups '("wheel" "netdev"
+                                        "audio" "video"))
+                (home-directory "/home/gazbit"))
+               %base-user-accounts))
+
+  ;; Add a bunch of window managers; we can choose one at
+  ;; the log-in screen with F1.
+  (packages (cons* ;; i3-wm i3status dmenu ;window managers
+                   nss-certs                      ;for HTTPS access
+                   %base-packages))
+
+  ;; Use the "desktop" services, which include the X11
+  ;; log-in service, networking with Wicd, and more.
+  (services %desktop-services)
+
+  ;; Allow resolution of '.local' host names with mDNS.
+  (name-service-switch %mdns-host-lookup-nss))
