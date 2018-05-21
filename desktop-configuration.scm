4b6876cd72e4be27ba3dfbd580ba34303a34084a
Gazaunga <Gazaunga@gmail.com>
2018-05-21 09:03:47 -0700

    Update desktop-configuration.scm

--- desktop-configuration.scm
+++ desktop-configuration.scm
@@ -42,7 +42,7 @@
 
   ;; Add a bunch of window managers; we can choose one at
   ;; the log-in screen with F1.
-  (packages (cons* ;; i3-wm i3status dmenu ;window managers
+  (packages (cons* ;; bspwm i3-wm i3status dmenu ;window managers
                    nss-certs                      ;for HTTPS access
                    %base-packages))
 
